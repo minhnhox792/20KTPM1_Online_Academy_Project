81d064c65db67f8990129eabe6f4ffa53323d284
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2023-01-01 10:48:50 +0700

    Category' CRUD

--- src/app/models/Category.js
+++ src/app/models/Category.js
@@ -1,22 +1,23 @@
 import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
-const ObjectId = Schema.Types.ObjectId;
 
 const Category = new Schema({
-  name: {
+  category: {
     type: String,
     required: true,
     unique: true,
+    default: 'none',
   },
   subCategories: {
     type: [
       {
-        type: ObjectId,
-        ref: 'SubCategory',
+        type: String,
+        unique: true,
+        default: [],
       },
     ],
-    default: []
+    default: [],
   },
 });
 
