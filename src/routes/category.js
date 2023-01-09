21ad34f4333a83c90b7c0ae1dae677ff221f3eb7
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2023-01-09 11:36:44 +0700

    Change css style

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import categoryController from '../app/controllers/categoryController.js';
-import authMiddleware from '../app/controllers/middleware/authMiddleware.js'
 const router = express.Router();
 
-router.get('/view/:id', authMiddleware.isAuthenticated, categoryController.view);
-router.get('/subCategory/:id', authMiddleware.isAuthenticated,  categoryController.subCategory);
-router.get('/nextSub/:id', authMiddleware.isAuthenticated,  categoryController.requestSubcatory);
+router.get('/view/:id', categoryController.view);
+router.get('/subCategory/:id', categoryController.subCategory);
+router.get('/nextSub/:id', categoryController.requestSubcatory);
 
 export default router;
